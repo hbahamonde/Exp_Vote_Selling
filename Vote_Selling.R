e570556caf54da3c262284b4e35af2c627420d61
Hector <bahamondeh@gmail.com>
2021-11-11 11:54:51 +0200

    wip

--- Vote_Selling.R
+++ Vote_Selling.R
@@ -4,6 +4,9 @@
 cat("\014")
 rm(list=ls())
 
+# K-adic Data Analyses
+# https://www.paulpoast.com/stata-software/4587316769
+
 # function to to recover mode
 getmode <- function(v) {
   uniqv <- unique(v)
@@ -147,11 +150,24 @@ v.selling.dat = dat[c(id.vars, v.selling.vars, socio.dem.vars)]
 # dropping obs that dont belong to the vote buying exp
 v.selling.dat <- subset(v.selling.dat, !is.na(vote_s.1.player.votanteOpartido))
 
+# extend voter's participant.code to the dyad: the common point between the parties is the voter.
+p_load(dplyr,tidyverse)
+# 1
+v.selling.dat$participant.code.dyad.1 = ifelse(v.selling.dat$vote_s.1.player.votanteOpartido == "votantes", v.selling.dat$participant.code, NA)
+v.selling.dat = v.selling.dat %>% group_by(vote_s.1.group.presupuesto) %>% fill(participant.code.dyad.1, .direction = "downup")
+# 2
+v.selling.dat$participant.code.dyad.2 = ifelse(v.selling.dat$vote_s.2.player.votanteOpartido == "votantes", v.selling.dat$participant.code, NA)
+v.selling.dat = v.selling.dat %>% group_by(vote_s.2.group.presupuesto) %>% fill(participant.code.dyad.2, .direction = "downup")
+# 3
+v.selling.dat$participant.code.dyad.3 = ifelse(v.selling.dat$vote_s.3.player.votanteOpartido == "votantes", v.selling.dat$participant.code, NA)
+v.selling.dat = v.selling.dat %>% group_by(vote_s.3.group.presupuesto) %>% fill(participant.code.dyad.3, .direction = "downup")
+
+
 # party.id.before.voter
 p_load(dplyr,tidyverse)
-v.selling.dat = v.selling.dat %>% group_by(vote_s.1.group.presupuesto) %>% fill(vote_s.1.player.tipoAoB); v.selling.dat$party.id.before.voter.1 = v.selling.dat$vote_s.1.player.tipoAoB 
-v.selling.dat = v.selling.dat %>% group_by(vote_s.2.group.presupuesto) %>% fill(vote_s.2.player.tipoAoB); v.selling.dat$party.id.before.voter.2 = v.selling.dat$vote_s.2.player.tipoAoB 
-v.selling.dat = v.selling.dat %>% group_by(vote_s.3.group.presupuesto) %>% fill(vote_s.3.player.tipoAoB); v.selling.dat$party.id.before.voter.3 = v.selling.dat$vote_s.3.player.tipoAoB 
+v.selling.dat = v.selling.dat %>% group_by(vote_s.1.group.presupuesto) %>% fill(vote_s.1.player.tipoAoB, .direction = "downup"); v.selling.dat$party.id.before.voter.1 = v.selling.dat$vote_s.1.player.tipoAoB 
+v.selling.dat = v.selling.dat %>% group_by(vote_s.2.group.presupuesto) %>% fill(vote_s.2.player.tipoAoB, .direction = "downup"); v.selling.dat$party.id.before.voter.2 = v.selling.dat$vote_s.2.player.tipoAoB 
+v.selling.dat = v.selling.dat %>% group_by(vote_s.3.group.presupuesto) %>% fill(vote_s.3.player.tipoAoB, .direction = "downup"); v.selling.dat$party.id.before.voter.3 = v.selling.dat$vote_s.3.player.tipoAoB 
 
 
 # ideo.distance dyadic
@@ -300,6 +316,17 @@ v.selling.dat$vote.intention.party.2.b = v.selling.dat$vote_s.2.group.n_votantes
 v.selling.dat$vote.intention.party.3.a = v.selling.dat$vote_s.3.group.n_votantes_A
 v.selling.dat$vote.intention.party.3.b = v.selling.dat$vote_s.3.group.n_votantes_B
 
+# % voters
+## A
+v.selling.dat$vote.intention.party.per.1.a = round((v.selling.dat$vote_s.1.group.n_votantes_A*100)/v.selling.dat$vote_s.1.group.n_votantes,0)
+v.selling.dat$vote.intention.party.per.2.a = round((v.selling.dat$vote_s.2.group.n_votantes_A*100)/v.selling.dat$vote_s.2.group.n_votantes,0)
+v.selling.dat$vote.intention.party.per.3.a = round((v.selling.dat$vote_s.3.group.n_votantes_A*100)/v.selling.dat$vote_s.3.group.n_votantes,0)
+## B
+v.selling.dat$vote.intention.party.per.1.b = round((v.selling.dat$vote_s.1.group.n_votantes_B*100)/v.selling.dat$vote_s.1.group.n_votantes,0)
+v.selling.dat$vote.intention.party.per.2.b = round((v.selling.dat$vote_s.2.group.n_votantes_B*100)/v.selling.dat$vote_s.2.group.n_votantes,0)
+v.selling.dat$vote.intention.party.per.3.b = round((v.selling.dat$vote_s.3.group.n_votantes_B*100)/v.selling.dat$vote_s.3.group.n_votantes,0)
+
+
 
 # budget dyadic
 v.selling.dat$budget.1.a = v.selling.dat$vote_s.1.group.presupuesto
@@ -324,16 +351,16 @@ v.selling.dat$points.this.round.2 = v.selling.dat$vote_s.2.player.puntos
 v.selling.dat$points.this.round.3 = v.selling.dat$vote_s.3.player.puntos
 
 
-# HERE
-
-
-
+## 1A
 v.selling.dat.1.a = subset(
   v.selling.dat, select = c(
     participant.code,
+    participant.code.dyad.1,
     role.1,
     participant.payoff,
     ideo.distance.1.a, 
+    vote.intention.party.1.a,
+    vote.intention.party.per.1.a,
     voter.offer.1.a,
     pivotal.voter.1.a, 
     voter.own.1.a,
@@ -341,8 +368,6 @@ v.selling.dat.1.a = subset(
     points.this.round.1
   )
 )
-
-
 v.selling.dat.1.a <- subset(v.selling.dat.1.a, role.1 == "Party A") # drop voters
 colnames(v.selling.dat.1.a)[colnames(v.selling.dat.1.a)=="role.1"] <- "Dyad" # changes name to dyad
 v.selling.dat.1.a$Dyad = as.factor(v.selling.dat.1.a$Dyad)
@@ -351,18 +376,236 @@ v.selling.dat.1.a = v.selling.dat.1.a %>% mutate(Dyad=recode(Dyad, "Party A" = "
 colnames(v.selling.dat.1.a) = sub(".1.*", "", colnames(v.selling.dat.1.a)) # remove extra characters in col names
 v.selling.dat.1.a$round = 1
 
-# do.
-## 1. compute vote share
-## 2. compute points cumula
+## 1B
+v.selling.dat.1.b = subset(
+  v.selling.dat, select = c(
+    participant.code,
+    participant.code.dyad.1,
+    role.1,
+    participant.payoff,
+    ideo.distance.1.b, 
+    vote.intention.party.1.b,
+    vote.intention.party.per.1.b,
+    voter.offer.1.b,
+    pivotal.voter.1.b, 
+    voter.own.1.b,
+    budget.1.b,
+    points.this.round.1
+  )
+)
+v.selling.dat.1.b <- subset(v.selling.dat.1.b, role.1 == "Party B") # drop voters
+colnames(v.selling.dat.1.b)[colnames(v.selling.dat.1.b)=="role.1"] <- "Dyad" # changes name to dyad
+v.selling.dat.1.b$Dyad = as.factor(v.selling.dat.1.b$Dyad)
+p_load(dplyr)
+v.selling.dat.1.b = v.selling.dat.1.b %>% mutate(Dyad=recode(Dyad, "Party A" = "Party A, Voter","Party B" = "Party B, Voter"))
+colnames(v.selling.dat.1.b) = sub(".1.*", "", colnames(v.selling.dat.1.b)) # remove extra characters in col names
+v.selling.dat.1.b$round = 1
+
+## 2A
+v.selling.dat.2.a = subset(
+  v.selling.dat, select = c(
+    participant.code,
+    participant.code.dyad.2,
+    role.2,
+    participant.payoff,
+    ideo.distance.2.a, 
+    vote.intention.party.2.a,
+    vote.intention.party.per.2.a,
+    voter.offer.2.a,
+    pivotal.voter.2.a, 
+    voter.own.2.a,
+    budget.2.a,
+    points.this.round.2
+  )
+)
+v.selling.dat.2.a <- subset(v.selling.dat.2.a, role.2 == "Party A") # drop voters
+colnames(v.selling.dat.2.a)[colnames(v.selling.dat.2.a)=="role.2"] <- "Dyad" # changes name to dyad
+v.selling.dat.2.a$Dyad = as.factor(v.selling.dat.2.a$Dyad)
+p_load(dplyr)
+v.selling.dat.2.a = v.selling.dat.2.a %>% mutate(Dyad=recode(Dyad, "Party A" = "Party A, Voter","Party B" = "Party B, Voter"))
+colnames(v.selling.dat.2.a) = sub(".2.*", "", colnames(v.selling.dat.2.a)) # remove extra characters in col names
+v.selling.dat.2.a$round = 2
+
+## 2B
+v.selling.dat.2.b = subset(
+  v.selling.dat, select = c(
+    participant.code,
+    participant.code.dyad.2,
+    role.2,
+    participant.payoff,
+    ideo.distance.2.b, 
+    vote.intention.party.2.b,
+    vote.intention.party.per.2.b,
+    voter.offer.2.b,
+    pivotal.voter.2.b, 
+    voter.own.2.b,
+    budget.2.b,
+    points.this.round.2
+  )
+)
+v.selling.dat.2.b <- subset(v.selling.dat.2.b, role.2 == "Party B") # drop voters
+colnames(v.selling.dat.2.b)[colnames(v.selling.dat.2.b)=="role.2"] <- "Dyad" # changes name to dyad
+v.selling.dat.2.b$Dyad = as.factor(v.selling.dat.2.b$Dyad)
+p_load(dplyr)
+v.selling.dat.2.b = v.selling.dat.2.b %>% mutate(Dyad=recode(Dyad, "Party A" = "Party A, Voter","Party B" = "Party B, Voter"))
+colnames(v.selling.dat.2.b) = sub(".2.*", "", colnames(v.selling.dat.2.b)) # remove extra characters in col names
+v.selling.dat.2.b$round = 2
+
+## 3A
+v.selling.dat.3.a = subset(
+  v.selling.dat, select = c(
+    participant.code,
+    participant.code.dyad.3,
+    role.3,
+    participant.payoff,
+    ideo.distance.3.a, 
+    vote.intention.party.3.a,
+    vote.intention.party.per.3.a,
+    voter.offer.3.a,
+    pivotal.voter.3.a, 
+    voter.own.3.a,
+    budget.3.a,
+    points.this.round.3
+  )
+)
+v.selling.dat.3.a <- subset(v.selling.dat.3.a, role.3 == "Party A") # drop voters
+colnames(v.selling.dat.3.a)[colnames(v.selling.dat.3.a)=="role.3"] <- "Dyad" # changes name to dyad
+v.selling.dat.3.a$Dyad = as.factor(v.selling.dat.3.a$Dyad)
+p_load(dplyr)
+v.selling.dat.3.a = v.selling.dat.3.a %>% mutate(Dyad=recode(Dyad, "Party A" = "Party A, Voter","Party B" = "Party B, Voter"))
+colnames(v.selling.dat.3.a) = sub(".3.*", "", colnames(v.selling.dat.3.a)) # remove extra characters in col names
+v.selling.dat.3.a$round = 3
+
+## 3B
+v.selling.dat.3.b = subset(
+  v.selling.dat, select = c(
+    participant.code,
+    participant.code.dyad.3,
+    role.3,
+    participant.payoff,
+    ideo.distance.3.b, 
+    vote.intention.party.3.b,
+    vote.intention.party.per.3.b,
+    voter.offer.3.b,
+    pivotal.voter.3.b, 
+    voter.own.3.b,
+    budget.3.b,
+    points.this.round.3
+  )
+)
+v.selling.dat.3.b <- subset(v.selling.dat.3.b, role.3 == "Party B") # drop voters
+colnames(v.selling.dat.3.b)[colnames(v.selling.dat.3.b)=="role.3"] <- "Dyad" # changes name to dyad
+v.selling.dat.3.b$Dyad = as.factor(v.selling.dat.3.b$Dyad)
+p_load(dplyr)
+v.selling.dat.3.b = v.selling.dat.3.b %>% mutate(Dyad=recode(Dyad, "Party A" = "Party A, Voter","Party B" = "Party B, Voter"))
+colnames(v.selling.dat.3.b) = sub(".3.*", "", colnames(v.selling.dat.3.b)) # remove extra characters in col names
+v.selling.dat.3.b$round = 3
+
+# stack up 3 games for A abd B dyads
+dat.v.s = data.frame(
+  rbind(v.selling.dat.1.a,
+        v.selling.dat.1.b,
+        v.selling.dat.2.a,
+        v.selling.dat.2.b,
+        v.selling.dat.3.a,
+        v.selling.dat.3.b
+        )
+  )
+
+# Dropping NAs
+dat.v.s = dat.v.s %>% drop_na()
+
+# change in payoff
+dat.v.s = dat.v.s %>%
+  group_by(participant.code) %>%
+  mutate(points.cumul.delta = points.this.round - lag(points.this.round))
 
 
 
+######################################################################### 
+# ************** M      O       D       E       L       S **************
+#########################################################################
 
 
+m1 = lm(voter.offer ~ vote.intention.party.per + points.cumul.delta + ideo.distance + budget + pivotal.voter + participant.code.dyad, dat.v.s)
+options(scipen=9999999)
+summary(m1)
+
+# Clustered Std Errors and Model info
+options(scipen=9999999) # turn off sci not
+p_load(sandwich,lmtest,DAMisc,lattice,latticeExtra)
+m1.clst.std.err = as.numeric(coeftest(m1, vcov. = vcovCL(m1, cluster = dat.v.s$participant.code.dyad, type = "HC0"))[,2])[1:6]
+m1.clst.t.test = c(as.numeric(coeftest(m1, vcov. = vcovCL(m1, cluster = dat.v.s$participant.code.dyad, type = "HC0"))[,3])[1:6])
+m1.clst.p.value = c(as.numeric(coeftest(m1, vcov. = vcovCL(m1, cluster = dat.v.s$participant.code.dyad, type = "HC0"))[,4])[1:6])
+custom.model.names.m1 = "Amount of Vote-Buying Offer"
 
 
-# K-adic Data Analyses
-# https://www.paulpoast.com/stata-software/4587316769
+## MODEL 1 PLOTS
+#mientras mas pierdo ayer, mas caro compro hoy
+m1.p1.d = data.frame(ggeffects::ggpredict(
+  model=m1,
+  terms=c("points.cumul.delta [all]"), 
+  vcov.fun = "vcovHC", 
+  vcov.type = "HC0")
+); m1.p1.d$group = "Points Cumul (delta)"
 
 
+#mientras mas votos a favor tengo, mas ofrezco
+m1.p2.d = data.frame(ggeffects::ggpredict(
+  model=m1,
+  terms=c("vote.intention.party.2 [all]"), 
+  vcov.fun = "vcovHC", 
+  vcov.type = "HC0")
+); m1.p2.d$group = "Vote Share (%)"
 
+# no importa la distancia ideologica
+m1.p3.d = data.frame(ggeffects::ggpredict(
+  model=m1,
+  terms=c("ideo.distance2 [all]"), 
+  vcov.fun = "vcovHC", 
+  vcov.type = "HC0")
+); m1.p3.d$group = "Spatial Distance"
+
+# no importa el budget del partido
+m1.p4.d = data.frame(ggeffects::ggpredict(
+  model=m1,
+  terms=c("budget [all]"), 
+  vcov.fun = "vcovHC", 
+  vcov.type = "HC0")
+); m1.p4.d$group = "Party's Budget"
+
+
+# pivotal voter
+m1.p5.d = data.frame(ggeffects::ggpredict(
+  model=m1,
+  terms=c("pivotal.voter [all]"), 
+  vcov.fun = "vcovHC", 
+  vcov.type = "HC0")
+); m1.p5.d$group = "Pivotal Voter"
+
+# plot (export by hand)
+m1.p.d = as.data.frame(rbind(m1.p1.d,m1.p2.d,m1.p3.d,m1.p4.d,m1.p5.d))
+m1.p.d$group = factor(m1.p.d$group, 
+                      levels = c("Vote Share (%)", 
+                                 "Points Cumul (delta)", 
+                                 "Spatial Distance", 
+                                 "Party's Budget",
+                                 "Pivotal Voter"))
+
+#m1.p.d$group = as.factor(m1.p.d$group)
+#m1.p.d$group <- relevel(m1.p.d$group, "Points Cumul (delta)")
+
+p_load(lattice, latticeExtra, DAMisc)
+m1plot = xyplot(predicted ~ x | group, 
+                scales=list(relation="free", rot=0),
+                data=m1.p.d, 
+                aspect = 1,
+                xlab = " ", 
+                ylab = "Amount of Vote-Buying Offer (points)", 
+                lower=m1.p.d$conf.low,
+                upper=m1.p.d$conf.high,
+                panel = panel.ci, 
+                zl=F, 
+                prepanel=prepanel.ci,
+                layout = c(5, 1) # columns, rows
+)
